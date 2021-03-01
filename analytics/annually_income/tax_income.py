c3ee23cc123da9c09b050bd67e8a500a99c8cd5f
flutterdjango <abc@gmail.com>
2021-03-02 01:14:09 +0900

    :bug: fix calc logix

--- analytics/annually_income/tax_income.py
+++ analytics/annually_income/tax_income.py
@@ -2,7 +2,7 @@
 import math
 
 
-class TaxIncome:
+class TaxInfo:
     def __init__(
             self,
             tax_by_percent: int,
@@ -22,20 +22,20 @@ class TaxIncomeManager:
 
     def __init__(self):
         self.tax_income_list = [
-            TaxIncome(5, 0, 1950000, 0),
-            TaxIncome(10, 1950000, 3300000, 97500),
-            TaxIncome(20, 3300000, 6950000, 427500),
-            TaxIncome(23, 6950000, 9000000, 636000),
-            TaxIncome(33, 9000000, 17800000, 1536000),
-            TaxIncome(40, 17800000, 40000000, 2796000),
-            TaxIncome(45, 40000000, math.inf, 4796000)
+            TaxInfo(5, 0, 1950000, 0),
+            TaxInfo(10, 1950000, 3300000, 97500),
+            TaxInfo(20, 3300000, 6950000, 427500),
+            TaxInfo(23, 6950000, 9000000, 636000),
+            TaxInfo(33, 9000000, 17800000, 1536000),
+            TaxInfo(40, 17800000, 40000000, 2796000),
+            TaxInfo(45, 40000000, math.inf, 4796000)
         ]
 
     def get_tax(self, income: int) -> int:
         range = self.get_range(income)
-        return (income - range.deduction) * range.tax_by_percent * 0.01
+        return income * range.tax_by_percent * 0.01 - range.deduction
 
-    def get_range(self, income: int) -> TaxIncome:
+    def get_range(self, income: int) -> TaxInfo:
         for tax_income in self.tax_income_list:
             gt_min_income = tax_income.gt_min_income
             max_income = tax_income.max_income
